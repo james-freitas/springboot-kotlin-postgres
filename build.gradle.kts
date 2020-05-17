df3ff9acdce8a31a39733a1b81293cc479093de0
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-05-17 17:12:46 -0300

    Remove lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,9 +34,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
